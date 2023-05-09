14263c3df484093351fc6be69a6fe0d7fd7f18ac
tboihang <tboihang20@gmail.com>
2023-05-09 14:08:56 -0700

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 """Print the alphabet in lowercase, not followed by a new line."""
-for ch in range(97, 123):
-    print("ASCII value: " + str(ch) + ", Character: ", chr(ch))
+for i in range(97, 123):
+    print(chr(i),end=")
