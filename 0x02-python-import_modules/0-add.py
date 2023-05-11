0eb0d739d918e5321df7e75385190a776f4c1eb4
tboihang <tboihang20@gmail.com>
2023-05-11 11:47:57 -0700

    0-add.

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """prints the result of the addition 1 + 2 = 3."""
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
